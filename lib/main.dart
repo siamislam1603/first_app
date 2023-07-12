e47e7a3277c99ece727f3e35673b5ac600779326
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2023-07-12 15:18:11 +0600

    Created widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello mlkxjl!')));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('First App'),
+        ),
+      ),
+    ),
+  );
 }

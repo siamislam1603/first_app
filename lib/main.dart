41db91a4b769386a72422726ac9c32a5cc885f6e
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2023-08-13 15:02:37 +0600

    Added blueprint of Expense & introduced enum

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:first_app/gradient_container.dart';
+import 'package:first_app/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(
-          colors: [Colors.deepPurple, Color.fromARGB(255, 143, 58, 183)],
-        ),
-      ),
+      home: Expenses(),
     ),
   );
 }
